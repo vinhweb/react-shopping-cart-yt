b9ca3ae95f956f6a9c6811760ef23d1ae4735e9a
vinhweb <vjhnguyen@gmail.com>
2024-06-30 14:53:06 +0700

    slideshow

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import Slider from "../components/slider/slider";
+import Slideshow from "../components/slider/slideshow";
 
 export default function Home(){
 	return (
 		<main>
 			<h1>Trang chủ</h1>
-			<Slider/>
+			{/*<Slider/>*/}
+			<Slideshow/>
 		</main>
 	)
 }
